14db20fd733cebb3b905ce2c2792a28114af17f0
tsiolth1 <tsiolth1@PHCHBS-L96733.EU.novartis.net>
2016-11-25 17:10:09 +0100

    2nd attempt

--- gitTest/src/gitTest/Main.java
+++ gitTest/src/gitTest/Main.java
@@ -1,11 +1,6 @@
- package gitTest;
-
 public class Main {
-	
-	public static void helloWorld() {
-	
-		System.out.println("Hello Pao");
-		
-
-	}
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
 }
